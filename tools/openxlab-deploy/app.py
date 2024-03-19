089c1f5cbca3b613cea752962a0ccd47b79730b5
houshaowei <houshaowei@pjlab.org.cn>
2024-03-19 13:27:48 +0800

    Deploy the InternLM2 course on OpenXLab

---
+++ tools/openxlab-deploy/app.py
@@ -0,0 +1,22 @@
+import gradio as gr
+import os
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer, AutoModel
+
+base_path = './internlm2-chat-7b'
+os.system(f'git clone https://code.openxlab.org.cn/OpenLMLab/internlm2-chat-7b.git {base_path}')
+os.system(f'cd {base_path} && git lfs pull')
+
+tokenizer = AutoTokenizer.from_pretrained(base_path,trust_remote_code=True)
+model = AutoModelForCausalLM.from_pretrained(base_path,trust_remote_code=True, torch_dtype=torch.float16).cuda()
+
+def chat(message,history):
+    for response,history in model.stream_chat(tokenizer,message,history,max_length=2048,top_p=0.7,temperature=1):
+        yield response
+
+gr.ChatInterface(chat,
+                 title="InternLM2-Chat-7B",
+                description="""
+InternLM is mainly developed by Shanghai AI Laboratory.  
+                 """,
+                 ).queue(1).launch()
