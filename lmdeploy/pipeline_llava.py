b2f5532f148577ff6a0d17be1f0bd234ba420926
Coder.AN <an.hongjun@foxmail.com>
2024-04-07 19:22:18 +0800

    coderan add pipeline_llava.py

---
+++ lmdeploy/pipeline_llava.py
@@ -0,0 +1,8 @@
+from lmdeploy import pipeline
+from lmdeploy.vl import load_image
+
+pipe = pipeline('liuhaotian/llava-v1.6-vicuna-7b')
+
+image = load_image('https://raw.githubusercontent.com/open-mmlab/mmdeploy/main/tests/data/tiger.jpeg')
+response = pipe(('describe this image', image))
+print(response)
