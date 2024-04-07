eb7fed4352278cf2d65b7194a4f9f3199151b98b
Coder.AN <an.hongjun@foxmail.com>
2024-04-07 17:31:54 +0800

    coderan add pipeline_transformer.py

---
+++ lmdeploy/pipeline_transformer.py
@@ -0,0 +1,18 @@
+import torch
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+tokenizer = AutoTokenizer.from_pretrained("/root/internlm2-chat-1_8b", trust_remote_code=True)
+
+# Set `torch_dtype=torch.float16` to load model in float16, otherwise it will be loaded as float32 and cause OOM Error.
+model = AutoModelForCausalLM.from_pretrained("/root/internlm2-chat-1_8b", torch_dtype=torch.float16, trust_remote_code=True).cuda()
+model = model.eval()
+
+inp = "hello"
+print("[INPUT]", inp)
+response, history = model.chat(tokenizer, inp, history=[])
+print("[OUTPUT]", response)
+
+inp = "please provide three suggestions about time management"
+print("[INPUT]", inp)
+response, history = model.chat(tokenizer, inp, history=history)
+print("[OUTPUT]", response)
