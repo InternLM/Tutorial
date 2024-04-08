4f8b6d17a8106902a92fdeb33307a9eeed99a52a
Coder.AN <an.hongjun@foxmail.com>
2024-04-08 11:37:24 +0800

    coderan add gradio_llava.py

---
+++ lmdeploy/gradio_llava.py
@@ -0,0 +1,15 @@
+import gradio as gr
+from lmdeploy import pipeline
+
+
+pipe = pipeline('liuhaotian/llava-v1.6-vicuna-7b')
+
+def model(image, text):
+    if image is None:
+        return [(text, "请上传一张图片。")]
+    else:
+        response = pipe((text, image)).text
+        return [(text, response)]
+
+demo = gr.Interface(fn=model, inputs=[gr.Image(type="pil"), gr.Textbox()], outputs=gr.Chatbot())
+demo.launch()   
