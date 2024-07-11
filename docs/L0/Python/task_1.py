2bc1e3099d85431d3f06d25eeceee5f749c4557e
Linx <18289482+smoryan@users.noreply.github.com>
2024-07-11 13:25:26 +0800

    Update and rename task_1 to task_1.py
    Task completed.

--- docs/L0/Python/task_1.py
+++ docs/L0/Python/task_1.py
@@ -1,4 +1,6 @@
-text = """
+from collections import Counter
+
+task_text = """
 Got this panda plush toy for my daughter's birthday,
 who loves it and takes it everywhere. It's soft and
 super cute, and its face has a friendly look. It's
@@ -9,5 +11,17 @@ so I got to play with it myself before I gave it
 to her.
 """
 
-def wordcount(text):
-    pass
+
+def wordcount(text: str):
+    text = text.lower()
+    text = (
+        text.replace("\n", " ")
+        .replace(".", " ")
+        .replace(",", " ")
+        .replace("  ", " ")
+        .strip()
+    )
+    return Counter(text.split(" "))
+
+
+print(dict(wordcount(task_text)))
