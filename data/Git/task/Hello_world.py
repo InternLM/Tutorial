eaefc90df53ca7294db51c34748d5885c0e1a9b4
jyf2100 <dvs@vip.sina.com>
2024-07-19 15:33:08 +0800

    Hello world

---
+++ data/Git/task/Hello_world.py
@@ -0,0 +1,32 @@
+import socket
+import re
+import gradio as gr
+ 
+# 获取主机名
+def get_hostname():
+    hostname = socket.gethostname()
+    match = re.search(r'-(\d+)$', hostname)
+    name = match.group(1)
+    
+    return name
+ 
+# 创建 Gradio 界面
+with gr.Blocks(gr.themes.Soft()) as demo:
+    html_code = f"""
+            <p align="center">
+            <a href="https://intern-ai.org.cn/home">
+                <img src="https://intern-ai.org.cn/assets/headerLogo-4ea34f23.svg" alt="Logo" width="20%" style="border-radius: 5px;">
+            </a>
+            </p>
+            <h1 style="text-align: center;">☁️ Welcome {get_hostname()} user, welcome to the ShuSheng LLM Practical Camp Course!</h1>
+            <h2 style="text-align: center;">😀 Let’s go on a journey through ShuSheng Island together.</h2>
+            <p align="center">
+                <a href="https://github.com/InternLM/Tutorial/blob/camp3">
+                    <img src="https://oss.lingkongstudy.com.cn/blog/202406301604074.jpg" alt="Logo" width="20%" style="border-radius: 5px;">
+                </a>
+            </p>
+
+            """
+    gr.Markdown(html_code)
+
+demo.launch()
